05589e024f7b85ebd099728637f4d24f567f3c76
BraedenC <braeden.c.connors@gmail.com>
2024-09-12 15:11:54 -0500

    Add project files.

---
+++ 02 IntroToOOP.cs
@@ -0,0 +1,14 @@
+﻿namespace CS221.Lecture2
+{
+    public static class IntroToOOP
+    {
+        private static void Main()
+        {
+            Movie m1 = new Movie();
+            m1.Title = "2001: A Space Odyssey";
+
+            Console.WriteLine(m1.Title);
+
+        }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,30 @@
+﻿namespace CS221.Lecture2
+{
+    public class Movie
+    {
+        private string title;
+        private int year;
+        private double rating;
+        private int year1 { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public int Year
+        {
+            get => year;
+            set => year = value;
+        }
+
+        public double Rating { get; private set; }
+    }
+}
