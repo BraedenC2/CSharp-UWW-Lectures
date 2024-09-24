fe632c7d1eb8b378bd7f4eb5f286fee029929c37
BraedenC <braeden.c.connors@gmail.com>
2024-09-24 16:44:33 -0500

    File writing

---
+++ FileIOPart1.cs
@@ -0,0 +1,40 @@
+﻿namespace Lectures
+{
+    public static class FileIOPart1
+    {
+        private static void FileWritingDemo()
+        {
+            // Only use relative paths!!:
+            string path = @"data\test1.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Exists");
+            }
+            else
+            {
+                Console.WriteLine("Does not exists");
+            }
+
+            try
+            {
+                using StreamWriter writer = new StreamWriter(path);
+                writer.WriteLine("Hello, World!"); // Overwrites existing content
+                writer.WriteLine("How are you?");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while writing to file.");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        private static void Main()
+        {
+
+            FileWritingDemo();
+
+
+        }
+    }
+}
