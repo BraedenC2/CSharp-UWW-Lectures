a1c504c27d4daf828f4a1d55f4d8a3a27d8b5786
BraedenC <braeden.c.connors@gmail.com>
2024-10-03 16:22:56 -0500

    Added Recursion class.

---
+++ Recursion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lectures {
+
+    public static class Recursion {
+
+        private static void Main() {
+
+            for ( int i = 0; i < 2000; i++ ) Console.WriteLine(Fibonacci(i));
+            
+        }
+
+        private static int Factorial( int n ) {
+
+            if ( n == 0 ) return 1;
+            return n * Factorial( n - 1 );
+
+        }
+
+        private static int Fibonacci( int n ) {
+            
+            if ( n <= 1 ) return n;
+
+            return Fibonacci( n - 1 ) + Fibonacci( n - 2 );
+
+        }
+    }
+}
